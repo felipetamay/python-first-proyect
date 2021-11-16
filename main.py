01015501250bab6bcd86da5ecd88f19a005f8598
felipetamay <93955492+felipetamay@users.noreply.github.com>
2021-11-16 08:21:37 -0500

    Print in python

---
+++ main.py
@@ -0,0 +1,15 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+message="Hola que tal"
+number_var=123
+float_var=123,342
+
+print(message)
+print(type(message))
+
+print(number_var)
+print(type(number_var))
+
+print(float_var)
+print(type(float_var))
